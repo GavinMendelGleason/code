e89f7c1bbedd17608ff3436ea37dc175f8f73a8d
Gavin Mendel-Gleason <jacobian@gmail.com>
2017-06-02 23:08:17 +0100

    Updating prototype ahead of major surgery - removing ABT structure

--- ocaml/typo/prototype.ml
+++ ocaml/typo/prototype.ml
@@ -1,18 +1,27 @@
 
 open Base
        
+(* 
+val oo : 'a 
+handy for filling holes 
+*)
+let oo = failwith "not Implemented"                        
+       
 type id = string
 
 type ty =
   TyVar of id
 | All of id * ty
-| Int of int
+| Int of unit
 | Id of id
-| Bool of bool
+| Arr of ty * ty
+| Bool of unit
 | Times of ty * ty
 | Plus of ty * ty
-| Ind of (id * ty list) list
 
+(* inductively defined types *)
+type ind = (id * ty list) list
+                 
 type op = id * term list
 and term =
   Op of op
@@ -32,22 +41,18 @@ let rec foldMaybe b f l =
      (match f x b with
         None -> None
       | Some y -> foldMaybe y f xs)
-
+let rec mapMaybe f l = Option.value_map ~f:f ~default:None l
+     
 let uncurry f (a,b) = f a b
 let rec lookup i rho =
-  match rho with
-    [] -> None
-  | (x , t) :: xs ->
-     if x == i
-     then Some t
-     else lookup i xs
+  List.find ~f:(fun (x , t) -> String.equal x i) rho
                  
-open List
 let rec unify x y rho =
   match x with
     Op (o1 , tl1) ->
       (match y with
-         Op (o2 , tl2) -> foldMaybe rho (uncurry unify) (combine tl1 tl2)
+         Op (o2 , tl2) ->
+         Option.find_map ~f:(foldMaybe rho (uncurry unify)) (List.zip tl1 tl2) 
        | Var j -> bind j x rho
        | Lam _ -> None)
   | Var i -> bind i y rho
@@ -59,30 +64,68 @@ let rec unify x y rho =
          (match bind q (Var r) rho with
             None -> None
           | Some rho' -> unify t s rho'))
-and bind id t rho =
-  match lookup id rho with
-    None -> Some ((id,t)::rho)
-  | Some s -> unify t s rho
+and bind (i : id) t rho : rho option =
+  match lookup i rho with
+    None -> Some ((i , t)::rho)
+  | Some (j , s) -> unify t s rho
 
 type program = id * id list * obj
 type programCtx = (id * ty list * ty) list
 type ctx = (id * ty) list
 
 let rec lookupPredicateType i n objGamma =
-  find objGamma (fun (j, tylist, ty) -> i == j && n == length tylist)
+  List.find ~f:(fun (j, tylist, ty) -> String.equal i j && n = List.length tylist) objGamma 
        
 let lookupTermType i gamma =
-  find (fun (x , ty) -> i == x) gamma 
-                              
-let rec infer x gamma objGamma =
+  List.find ~f:(fun (x , ty) -> String.equal i x) gamma 
+
+let rec tyeq s t =
+  match s , t with
+  | TyVar id1 , TyVar id2 -> String.equal id1 id2
+  | All (is1 , ty1) , All (is2 , ty2) -> String.equal is1 is2 && tyeq ty1 ty2 
+  | Int () , Int () -> true
+  | Id i , Id j -> String.equal i j
+  | Arr (d1 , r1) , Arr (d2 , r2) -> tyeq d1 d2 && tyeq r1 r2
+  | Bool () , Bool () -> true
+  | Times (a1 , b1) , Times (a2 , b2) -> true
+  | Plus (a1 , b1) , Plus (a2 , b2) -> true
+  | _ -> false
+            
+(* No longer modular when we add this function, potentially could parameterise it *)
+let rec opType op tl gamma objGamma =
+  if String.equal op "app"
+  then let tylisto = mapMaybe (fun t -> infer t gamma objGamma) tl in
+       match tylisto with
+         None -> None
+       | Some tl -> let len = List.length tl in
+                    if len = 0
+                    then None
+                    else if len = 1
+                    then Some (List.hd_exn tl)
+                    else let fty = List.hd_exn tl in
+                         let xtyl = List.tl_exn tl in
+                         foldMaybe (Some fty)
+                                   (fun ofty vty ->
+                                     match ofty with
+                                     | None -> None
+                                     | Some (Arr dom range) ->
+                                        if tyeq dom vty
+                                        then ?
+                                        else None
+                                     | Some _ -> None
+                                   ) xtyl 
+  else (* Assume 'op' is a constructor *)
+    ??
+            
+and infer x gamma objGamma =
   match x with
     Op (o , tl) ->
-    let m = length tl in
-    (match lookupTermType o m gamma with
+    let m = List.length tl in
+    (match opType o tl gamma objGamma with
        None -> None
-     | Some (tylist, ty) -> Some true)
-  | Var i -> Some true
-  | Lam (i , t) -> Some true
+     | Some (tylist, ty) -> oo)
+  | Var i -> oo
+  | Lam (i , t) -> oo
 and checkObj x ty gamma objGamma =
   match x with
     Op (o , tl) -> true
@@ -97,3 +140,12 @@ let rec solve exp rho pro =
   | _ -> rho
 
  *)                          
+
+
+
+(* Examples 
+
+Op[ap ; f ; g]
+
+Op
+ *) 
