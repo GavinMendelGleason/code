53d8aa21a1bd99a230ef311ffa2ed757d1b5fa71
Gavin Mendel-Gleason <jacobian@gmail.com>
2017-06-13 14:35:24 +0100

    Adding OWL papepr

--- ocaml/typo/prototype.ml
+++ ocaml/typo/prototype.ml
@@ -1,13 +1,22 @@
 
 open Base
+
+let uncurry f (a,b) = f a b
+let curry f a b = f (a,b)
        
 type id = string
 type var = string * int
 
 let eqId i j = String.equal i j
-                      
+
+(* inductively defined types *)
+type inductiveName = id
+type inductiveTypeParameters = id list
+type inductiveType = inductiveName * inductiveTypeParameters
+
 type ty =
   | TyVar of id
+  | Ind of inductiveType
   | Arr of ty * ty
   | T of unit
   | Int of unit
@@ -15,12 +24,24 @@ type ty =
   | Times of ty * ty
   | Plus of ty * ty
 
-(* inductively defined types *)
-type inductiveName = id
-type inductiveTypeParameters = id list
+let (>>=) = Option.(>>=)
+                   
+let rec eqTy s t =
+  match s , t with
+  | TyVar id1 , TyVar id2 -> eqId id1 id2
+  | T () , T () -> true
+  | Int () , Int () -> true
+  | Arr (d1 , r1) , Arr (d2 , r2) -> eqTy d1 d2 && eqTy r1 r2
+  | Bool () , Bool () -> true
+  | Times (a1 , b1) , Times (a2 , b2) -> true
+  | Plus (a1 , b1) , Plus (a2 , b2) -> true
+  | Ind (i1 , il1) , Ind (i2 , il2) -> eqId i1 i2 && Option.value_map (List.zip il1 il2) ~default:false ~f:(List.for_all ~f:(uncurry eqId)) 
+  | _ -> false
+
+(* constructors of inductive type *) 
 type constructor = (id * ty list)
-                             
-type ind = inductiveName * inductiveTypeParameters * constructor list
+(* inductive type definition *)                             
+type ind = inductiveType * constructor list
 
 type typedefCtx = ind list
        
@@ -48,9 +69,10 @@ let fresh (x : id) : var =
 let printVar ((name , i) : var) : string = Printf.sprintf "%s.%d" name i
         
 let up ((name , i) : var)  (n : int) : var = (name , i+n)
-let down ((name , i) : var) (n : int) : var = if Int.equal i 0
-                                              then failwith "Impossible to lower zero" 
-                                              else (name, i-n)
+let down ((name , i) : var) (n : int) : var =
+  if Int.equal i 0
+  then failwith "Impossible to lower zero" 
+  else (name, i-n)
 let eqVar ((_ , i) : var) ((_ , j) : var) : bool = Int.equal i j
                                      
 let rec subst (x : var) (s : term) (t : term) : term =
@@ -63,7 +85,9 @@ let rec subst (x : var) (s : term) (t : term) : term =
   | FVar fv -> FVar fv
   | Lam (i, t) -> Lam (i , subst (up x 1) s t)
         
-let rec openTerm (name : id) (t : term) : term = subst (name , 0) (FVar (fresh name)) t
+let rec openTerm (name : id) (t : term) : (term * var) =
+  let fv = fresh name
+  in (subst (name , 0) (FVar fv) t, fv)
 
 let closeTerm (v : var) (t : term) : term =
   let rec closeTerm_aux (v : var) (bv : var) (t : term) : term =
@@ -104,8 +128,6 @@ let rec everyOk l : bool = match l with
       | Error _ -> false
       | Ok _ -> everyOk xs)
                                         
-let uncurry f (a,b) = f a b
-
 let rec lookup i rho =
   List.find ~f:(fun (x , t) -> eqVar x i) rho
                  
@@ -139,7 +161,7 @@ and bind (i : var) t rho : rho option =
 
 type program = id * id list * obj
 type programCtx = (id * ty list * ty) list
-type ctx = (id * ty) list
+type ctx = (var * ty) list
 
 let rec lookupPredicateType i n objGamma =
   List.find ~f:(fun (j, tylist, ty) -> eqId i j && n = List.length tylist) objGamma 
@@ -147,67 +169,133 @@ let rec lookupPredicateType i n objGamma =
 let lookupTermType i gamma =
   List.find ~f:(fun (x , ty) -> eqId i x) gamma 
 
-let lookupDefinition o ctx : constructor option =
-  List.find_map ctx ~f:(fun (x , tyl , constr) ->
-                           List.find ~f:(fun (c , tyl') -> eqId o c) constr)
-            
-let rec tyeq s t =
-  match s , t with
-  | TyVar id1 , TyVar id2 -> String.equal id1 id2
-  | T () , T () -> true
-  | Int () , Int () -> true
-  | Arr (d1 , r1) , Arr (d2 , r2) -> tyeq d1 d2 && tyeq r1 r2
-  | Bool () , Bool () -> true
-  | Times (a1 , b1) , Times (a2 , b2) -> true
-  | Plus (a1 , b1) , Plus (a2 , b2) -> true
-  | _ -> false
-
+       
 type typeError =
   | NoConstructor of id
   | BadConstructorArguments of typeError list
   | WrongNumberOfArgumentsToConstructor of term list * ty list 
   | ApplicationOfNonFunctionType of term * ty
+  | FreeVariableWithNoType of var * ctx
+  | FreeVariableWithWrongType of var * ty * ty
+  | LambdaNotFunction of term * ty
+  | TypesNotUnifiable of ty * ty
 
+let lookupDefinition o (ctx : typedefCtx) : constructor option =
+  List.find_map ctx ~f:(fun (it , constr) ->
+                          List.find ~f:(fun (c , tyl') -> eqId o c) constr)
+
+let lookupIndType o (ctx : typedefCtx) : inductiveType option =
+  Option.value_map ~default:None ~f:(fun (it , _) -> Some it)
+                   (List.find ctx ~f:(fun (x , constr) ->
+                                List.exists ~f:(fun (c , tyl') -> eqId o c) constr))
+                
+                    
 let failures lst = List.fold lst ~init:[] ~f:(fun r x -> match x with | Ok x -> r | Error e -> e :: r)
-                                             
+
+let (>>=) = Result.(>>=)
+let (>>=*) = Option.(>>=)
+
+let rec unifyType (tya : ty) (tyb : ty) ctx : ((ctx * ty) , typeError) Result.t =
+  match tya, tyb with
+  | Ind tyla, Ind tylb -> map2 tyla tylb ~f:(fun tya tyb -> unifyType tyla tylb
+  | Arr (_,_), Arr (_,_) -> (??)
+  | T () , T () -> Ok (T ())
+  | Int () , Int () -> Ok (Int ())
+  | Bool () , Bool () -> Ok (Bool ())
+  | Times (_,_) , Times (_,_) -> (??)
+  | Plus (_,_) , Plus (_,_) -> ??
+  | TyVar _, TyVar _ -> ??
+  | TyVar _, _ -> ??
+  | _, TyVar _ -> ??
+  | _ , _ -> Error (TypesNotUnifiable (tya,tyb))
+                   
 (* Inverts the meaning of option *)
 let rec checkFunType (fty : ty) (tylst : ty list) : (unit , typeError) Result.t =
+  ??
+and applyFunType (fty : ty) (tylst : ty list) : (ty , typeError) Result.t =
   ??
 and checkTermType (s : term) (ty : ty) gamma (ctx : typedefCtx) : (unit , typeError) Result.t =
   match s with
   | Fun (o , tl) ->
-     (match lookupDefinition o ctx with 
-      | None -> Error (NoConstructor o)
-      | Some (_ , tyl) ->
-         (match List.zip tl tyl with
-          | None -> Error (WrongNumberOfArgumentsToConstructor (tl , tyl))
-          | Some lst ->
-             let errlist = failures (List.map lst (fun (t,ty) -> checkTermType t ty gamma ctx))
-             in match errlist with
-                | [] -> Ok ()
-                | _ -> Error (BadConstructorArguments errlist)
-         )
-     )
+     Result.of_option (lookupDefinition o ctx)
+               (NoConstructor o) >>= (fun (_ , tyl) ->
+     Result.of_option (List.zip tl tyl)
+               (WrongNumberOfArgumentsToConstructor (tl , tyl)) >>= (fun lst ->
+     let errlist = failures (List.map lst (fun (t,ty) -> checkTermType t ty gamma ctx))
+     in match errlist with
+        | [] -> Ok ()
+        | _ -> Error (BadConstructorArguments errlist)))
   | App (f , tl) ->
-     let fty = inferTermType f gamma ctx in
-     let tlty = List.map tl ~f:(fun t -> inferTermType t gamma ctx)
-     in checkFunType fty flty     
+     let ftye = inferTermType f gamma ctx in
+     (* Monadic error fold of type inference over arguments *)
+     let xtyle = List.fold tl ~init:(Ok [])
+                           ~f:(fun r t ->
+                             match r with
+                             | Error e -> Error e
+                             | Ok tyl -> 
+                                match inferTermType t gamma ctx with
+                                | Ok ty -> Ok (ty :: tyl)
+                                | Error e -> Error e)
+     in ftye >>= (fun fty ->
+        xtyle >>= (fun xtyl ->
+        checkFunType fty xtyl))
      (* 
      (match ty with
       | Arr (aty,bty) -> (??)
       | _ -> Error (ApplicationOfNonFunctionType (s,ty))
       *)
-  | BVar _ -> (??)
-  | FVar _ -> (??)
-  | Lam (_,_) -> (??)                   
-and inferTermType (s : term) gamma ctx : (unit , typeError) Result.t =
+  | BVar _ -> failwith "Should not encounter bound var in type-checking 'checkTermType'"
+  | FVar fv ->
+     (match lookup fv gamma with
+      | None -> Error (FreeVariableWithNoType (fv , gamma))
+      | Some (_,ty') -> if eqTy ty ty'
+                        then Ok ()
+                        else Error (FreeVariableWithWrongType (fv , ty , ty')))
+  | Lam (x,t) ->
+     (match ty with
+      | Arr (tyx, tyy) ->
+         let (ot, v) = openTerm x t
+         in checkTermType ot tyy ((v, tyx) :: gamma) ctx
+      | _ -> Error (LambdaNotFunction (s , ty)))                   
+and inferTermType (s : term) gamma ctx : (ty , typeError) Result.t =
   match s with
-  | Fun _ -> (??)
-  | App (_,_) -> (??)
-  | BVar _ -> (??)
-  | FVar _ -> (??)
-  | Lam (_,_) -> (??)
-  		         
+  | Fun (i, tl) ->
+     Result.of_option (lookupDefinition i ctx) (NoConstructor i) >>= (fun (i, tyl) ->
+     Result.of_option (List.zip tl tyl)
+               (WrongNumberOfArgumentsToConstructor (tl , tyl)) >>= (fun lst ->
+     let errlist = failures (List.map lst (fun (t,ty) -> checkTermType t ty gamma ctx))
+     in match errlist with
+        | [] -> Result.of_option (lookupIndType i ctx >>=* (fun x -> 
+                                  Some (Ind x)))
+                                 (NoConstructor i)
+        | _ -> Error (BadConstructorArguments errlist)))
+  | App (f , x) ->
+     let ftye = inferTermType f gamma ctx in
+     (* Monadic error fold of type inference over arguments *)
+     let xtyle = List.fold tl ~init:(Ok [])
+                           ~f:(fun r t ->
+                             match r with
+                             | Error e -> Error e
+                             | Ok tyl -> 
+                                match inferTermType t gamma ctx with
+                                | Ok ty -> Ok (ty :: tyl)
+                                | Error e -> Error e)
+     in ftye >>= (fun fty ->
+        xtyle >>= (fun xtyl ->
+        applyFunType fty xtyl))
+  | BVar _ -> failwith "Should not encounter bound var in type-checking 'checkTermType'"
+  | FVar fv ->
+     (match lookup fv gamma with
+      | None -> Error (FreeVariableWithNoType (fv , gamma))
+      | Some (_,ty') -> Ok ty')
+  | Lam (x,t) ->
+     (match ty with
+      | Arr (tyx, tyy) ->
+         let (ot, v) = openTerm x t
+         in checkTermType ot tyy ((v, tyx) :: gamma) ctx
+      | _ -> Error (LambdaNotFunction (s , ty)))                   
+
+(* 
 let rec opType op tl gamma objGamma =
   if String.equal op "app"
   then let tylisto = mapMaybe (fun t -> infer t gamma objGamma) tl in
@@ -224,15 +312,13 @@ let rec opType op tl gamma objGamma =
                                    (fun ofty vty ->
                                      match ofty with
                                      | None -> None
-                                     | Some (Arr dom range) ->
+                                     | Some (Arr (dom,range)) ->
                                         if tyeq dom vty
-                                        then ?
+                                        then ??
                                         else None
                                      | Some _ -> None
                                    ) xtyl 
   else (* Assume 'op' is a constructor *)
-    ??
-            
 and infer x gamma objGamma =
   match x with
     Op (o , tl) ->
@@ -247,7 +333,8 @@ and checkObj x ty gamma objGamma =
   | Op (o , tl) -> true
   | Var i -> true
   | Lam (i , t) -> true
-  
+ *)
+                   
                                 
 (* 
 let rec solve exp rho pro =
